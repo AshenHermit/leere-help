aa76b6b38ce829cc8ff37adec539e56249d950d7
AshenHermit <nameless.voice.x@gmail.com>
2024-11-24 23:14:38 +0300

    chore: comments

--- backend/staff_portal/lib/auth.py
+++ backend/staff_portal/lib/auth.py
@@ -5,29 +5,46 @@ from ..models import *
 SECRET_KEYWORD = "dreammomentsparklesinmyeyespastelpinkfoxscintillatingfluffypawsreachflowerymomentreactlotusmagicsingingstarrydaytimearchperfectinsteadninesecondseversoftenough"
 
 def get_authorized_user(request: HttpRequest):
+    """для полученя текущего пользователя для текущего запроса"""
+    # по дефолту пусть user будет None
     user = None
     try:
+        # получаем куки "auth", которая должна хранить токен
         auth_token = request.COOKIES.get("auth")
+        # расшифровываем json токен с помощью jwt и получаем dict который зашифровали при авторизации
         token_data = jwt.decode(auth_token, SECRET_KEYWORD, algorithms=["HS256"])
 
+        # получаем запись пользователя в бд, по логину из данных токена
         user = User.objects.get(login=token_data["login"])
+        # кидаем ошибку если пользователь не найден
         if not user: raise Exception("no such user")
 
+        # получаем из данных токена пароль
         password = token_data["password"]
+        # проверяем соответствие пароля в токене к паролю из истинного пароля пользователя из бд 
         if user.password != password: raise Exception("wrong password")
 
     except:
         raise Exception("not authorized")
     
+    # если все норм, возвращаем объект пользователя
     return user
 
 def get_authorized_user_by_credentials(login, password):
-    user = User.objects.get(login=login)
-    if not user: raise Exception("no such user")
-    if user.password != password: raise Exception("wrong password")
+    """для получения пользователя по логину + проверка пароля.  
+    по сути работает как get_authorized_user, только эта функция читает не токен из куки, 
+    а реквизиты для входа в аргументы функции"""
 
+    # получаем пользователя по логину
+    user = User.objects.get(login=login)
+    # если пользователь не найден кидаем ошибку
+    if not user: raise Exception("no such user")
+    # проверяем пароль
+    if user.password != password: raise Exception("wrong password")
+    # если все хорошо возвращаем пользователя
     return user
 
 def generate_token_for_user(user:User):
+    """зашифровывает реквизиты для входа в аккаунт (логин пароль), получая на выходе токен"""
     token = jwt.encode({"login": user.login, "password": user.password}, SECRET_KEYWORD, algorithm="HS256")
     return token
--- backend/staff_portal/lib/tasks.py
+++ backend/staff_portal/lib/tasks.py
@@ -13,37 +13,59 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, JsonResponse, HttpRequest
 
 def get_available_departments(user:User):
+    """получает список доступных для чтения отделов"""
+
+    # получаем все дочерние отделы того отдела к которому привязан пользователь
     descendants: List[Department] = user.department.get_descendants(True)
     return descendants
 
 def get_available_tasks(user:User) -> List[Task]:
+    """используя `get_available_departments`, собирает из всех доступных отделов все задачи"""
+
     tasks = []
+    # получаем доступные отделы
     descendants: List[Department] = get_available_departments(user)
+    # перечисляем отделы
     for depart in descendants:
+        # получаем задачи отдела
         dep_tasks = Task.objects.filter(department=depart)
+        # добавляем их к общему списку
         tasks += dep_tasks
     return tasks
 
 def get_tasks_map(user: User, tasks: List[Task]):
+    """для преобразования списка задач в формат который читается фронт-эндом и используется на странице на стороне js"""
     tasks_map = {}
+    # перечисляем задачи
     for task in tasks:
-        tasks_map[task.pk] = {
+        # собираем отчеты задачи
+        reports = {}
+        for report in Report.objects.filter(task=task):
+            # добавляем их в dict, в которой "ключ" -> "значение" это "pk отчета" -> "текст отчета"
+            reports[report.pk] = report.content
+        
+        # 
+        task_data = {
             "pk": task.pk,
             "completed": task.completed,
             "title": task.title,
             "description": task.description,
             "department_pk": task.department.pk,
-            "can_edit": can_edit_task(user, task),
-            "can_add_report": can_add_report(user, task),
-            "reports": {}
+            "can_edit": can_edit_task(user, task), # чтобы решить отрисовывать ли элементы редактирования или нет
+            "can_add_report": can_add_report(user, task), # чтобы решить отрисовывать ли элементы добавления отчета
+            "reports": reports,
         }
-        for report in Report.objects.filter(task=task):
-            tasks_map[task.pk]["reports"][report.pk] = report.content
+        # добавляем в общий dict задач
+        tasks_map[task.pk] = task_data
             
     return tasks_map
 
 def can_edit_task(user:User, task:Task):
-    return task.author == user and user.position.level == 0 or 1
+    """проверка на то может ли пользователь редактировать/удалять задачу"""
+    # только автор задачи может ее редактировать
+    return task.author == user
 
 def can_add_report(user:User, task:Task):
+    """проверка на то может ли пользователь создать отчет"""
+    # все из отдела которому принадлежит задача, могут создавать отчеты
     return user.department == task.department
